a89d00b8f1665ff27d39c030988424c9398fe875
KADRDulmin <115799307+KADRDulmin@users.noreply.github.com>
2023-06-24 10:17:54 +0530

    New Update

---
+++ greeenEats/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace greeenEats
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void top_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2ControlBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label24_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2CircleButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ greeenEats/signinform.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace greeenEats
+{
+    public partial class signinform : Form
+    {
+        public signinform()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2ImageButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void guna2HtmlLabel2_Click(object sender, EventArgs e)
+        {
+            loginpage objl = new loginpage();
+            objl.Show();
+            this.Hide();
+        }
+
+        private void create_acc_btn_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+            private void create_acc_Click(object sender, EventArgs e)
+            {
+            IConfiguration configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json")
+                 .Build();
+
+            string server = configuration["Server"];
+            string database = configuration["Database"];
+            string username = configuration["Username"];
+            string password = configuration["Password"];
+
+            MySQLConnector connector = new MySQLConnector(server, database, username, password);
+
+            if (connector.OpenConnection())
+            {
+                string enteredFirstName = guna2TextBox1.Text;
+                string enteredLastName = guna2TextBox2.Text;
+                string email = guna2TextBox3.Text;
+                string enteredPassword = guna2TextBox4.Text;
+                string confirmedPassword = guna2TextBox5.Text;
+
+                // Validate the input and check if passwords match
+                if (string.IsNullOrWhiteSpace(enteredFirstName) ||
+                    string.IsNullOrWhiteSpace(enteredLastName) ||
+                    string.IsNullOrWhiteSpace(email) ||
+                    string.IsNullOrWhiteSpace(enteredPassword) ||
+                    string.IsNullOrWhiteSpace(confirmedPassword))
+                {
+                    MessageBox.Show("Please fill in all the fields.");
+                }
+                else if (enteredPassword != confirmedPassword)
+                {
+                    MessageBox.Show("Passwords do not match.");
+                }
+                else
+                {
+                    // Hash the password
+                    string hashedPassword = HashPassword(enteredPassword);
+                    DateTime currentDateTime = DateTime.Now;
+                       
+                    // Insert the user into the database
+                    string query = $"INSERT INTO user (first_name, last_name, email, password, created_at, updated_at) " +
+                                   $"VALUES ('{enteredFirstName}', '{enteredLastName}', '{email}', '{hashedPassword}', {currentDateTime},{currentDateTime})";
+
+                    MySqlCommand command = connector.CreateCommand(query);
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Account created successfully!");
+                        loginpage objlog = new loginpage();
+                        objlog.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to create account. Please try again.");
+                    }
+                }
+
+                connector.CloseConnection();
+            }
+            else
+            {
+                MessageBox.Show("Unable to establish a connection to the database. Please try again later.");
+            }
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //username
+        }
+
+        private void guna2TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            //phoneNumber
+        }
+
+        private void guna2TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            //email
+        }
+
+        private void guna2TextBox4_TextChanged(object sender, EventArgs e)
+        {
+            //password
+        }
+
+        private void guna2TextBox5_TextChanged(object sender, EventArgs e)
+        {
+            //confirm password
+        }
+
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Add code here for label1_Click event
+        }
+    }
+}
